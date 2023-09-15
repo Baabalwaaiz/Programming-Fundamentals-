d9b6618020d3a85c80dc29dd8c387ae7cf85ef6b
Baabalwaaiz <82728750+Baabalwaaiz@users.noreply.github.com>
2023-09-15 20:36:33 +0000

    stringstest1

---
+++ testarea.py
@@ -0,0 +1,12 @@
+a = "My first string"
+b = 'Another test string that I have defined'
+c = "This is Sal's string"
+d = 'My favorite word is "asparagus", what is yours'
+math_string = "3+4*2"
+expression_string = "a+ ' '+b+' tiger'"
+len(a)
+len(math_string)
+a_with_b = a+b
+a.split(' ')
+eval(math_string)
+eval(math_string + "1")
